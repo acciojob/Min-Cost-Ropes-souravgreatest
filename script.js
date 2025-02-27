96f8c6359809c0fd0a8e4dd7ca7ccdc645bb672b
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-02-27 16:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 function minCost(arr) {
-    const pq = new MinPriorityQueue({ priority: x => x });
-    
-    // Insert all elements into the priority queue
-    arr.forEach(num => pq.enqueue(num));
-    
+    arr.sort((a, b) => a - b);
     let cost = 0;
-    
-    // While there's more than one element in the queue
-    while (pq.size() > 1) {
-        const a = pq.dequeue().element;
-        const b = pq.dequeue().element;
-        
-        const sum = a + b;
+    while (arr.length > 1) {
+        let sum = arr[0] + arr[1];
         cost += sum;
-        
-        pq.enqueue(sum);
+        arr.splice(0, 2);
+        arr.push(sum);
+        arr.sort((a, b) => a - b);
     }
-    
     return cost;
 }
-/*
-
-1 2 3 4 5
-5 5 5 -> 10
-10 5  -> 10
-15
-
-*/
-
-module.exports=mincost;
